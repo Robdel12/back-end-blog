c36493fbbb63013682582fb3219d45456de852db
Robdel12 <robertdeluca19@gmail.com>
2015-08-04 22:28:28 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
-  protected
+  # protected
 
   def authenticate_user_from_token!
     puts "this work?"
